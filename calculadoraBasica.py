e397a27351ebd5e3e6d9c2f806b864f98ffa6c49
recapitalista <lozanonicolas.e@gmail.com>
2024-04-27 16:47:01 -0300

    calculadora basica v3

--- calculadoraBasica.py
+++ calculadoraBasica.py
@@ -1,19 +1,22 @@
-cuenta = input("Elegir tipo de operacion(suma,resta,multiplicaicon,division): ")
+cuenta = input("Elegir tipo de operacion(suma,resta,multiplicaicon,division,potencia): ").lower()
 num1 = float(input("Insertar Primer Numero: "))
 num2 = float(input("Insertar Segundo Numero: "))
 
 if cuenta == "suma" :
     resultado = num1 + num2
 elif cuenta == "resta" :
-    resultado = num1 + num2
+    resultado = num1 - num2
 elif cuenta == "multiplicacion" :
     resultado = num1 * num2
 elif cuenta == "division" :
     if num2 != 0 :
         resultado = num1 / num2
     else :
-        resultado = "No es posible dividir por cero"
+        resultado = "ERROR - No es posible dividir por cero"
+elif cuenta == "potencia" :
+    resultado = num1 ** num2
 else :
-    print("No existe esa opcion")
+    resultado = "ERROR - No existe esa opcion"
     
-print('El resultado es: ' + resultado)
+resultado = f'El resultado es: {resultado}'
+print(resultado)
